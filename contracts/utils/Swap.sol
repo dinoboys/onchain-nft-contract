cee30383baeedeb8d7b9f9968b9bd6231b2d7636
rzoktan <rz.oktan@gmail.com>
2023-06-03 13:41:26 +0700

    add WETH and swap sol

---
+++ contracts/utils/Swap.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+// Inspired by https://solidity-by-example.org/defi/uniswap-v2/
+
+pragma solidity ^0.8.17;
+
+// This is not a full swapping contract, but an implementation of how to interact with uniswap or a uniswap clone
+contract TestUniswap {
+    address private constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant TOKEN = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+
+    function swap(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _to
+    ) external {
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
+
+        address[] memory path;
+        if (_tokenIn == TOKEN || _tokenOut == TOKEN) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = TOKEN;
+            path[2] = _tokenOut;
+        }
+
+        IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            _to,
+            block.timestamp
+        );
+    }
+}
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
