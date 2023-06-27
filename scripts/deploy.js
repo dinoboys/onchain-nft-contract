1d81aa6e6c8c50beb47a53eb3fd461b9d1c5004b
rzoktan <rz.oktan@gmail.com>
2023-06-27 07:07:59 +0700

    save new

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,18 +8,20 @@ async function main() {
   );
   const descriptorContract = await descriptorFactory.deploy();
   await descriptorContract.deployed();
-  console.log("ERC721ADescriptor was deployed to:", ERC721ADescriptor.address);
+  console.log("ERC721ADescriptor was deployed to:", descriptorContract.address);
 
+  console.log("-----------------------");
+  console.log("Deploye nfts contract ...");
   const nftFactory = await hre.ethers.getContractFactory("FlappyOwlV1");
-  const nftContract = await nftFactory.deploy(ERC721ADescriptor.address);
+  const nftContract = await nftFactory.deploy(descriptorContract.address);
 
   await nftContract.deployed();
   console.log("FlappyOwlV1 Nft was deployed to:", nftContract.address);
-  console.log("verifying the contract ...");
+  console.log("-----------------------");
   // args represent contract constructor arguments
-  const args = [ERC721ADescriptor.address];
+  const args = [descriptorContract.address];
   await verify(nftContract.address, args, "ERC-721/FlappyOwlV1", "FlappyOwlV1");
-  console.log("-----------------");
+  console.log("-----------------------");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
